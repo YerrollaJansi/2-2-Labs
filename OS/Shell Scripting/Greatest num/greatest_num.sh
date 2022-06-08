004922635526c4580622ebcf87aeaac34c3eb125
YerrollaJansi <97152015+YerrollaJansi@users.noreply.github.com>
2022-06-08 07:15:44 +0530

    Greatest num

---
+++ OS/Shell Scripting/Greatest num/greatest_num.sh
@@ -0,0 +1,13 @@
+echo "enter a, b and c"
+read a
+read b
+read c
+if [ $a -gt $b ] && [ $a -gt $c ]
+then
+    echo "$a is greater"
+elif [ $b -gt $a ] && [ $b -gt $c ]
+then 
+    echo "$b is greater" 
+else
+    echo "$c is greater"
+fi
